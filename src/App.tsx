7eb2633408a4250f155c15069c5b6b9a1a7469ce
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-08-20 15:07:49 -0300

    Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
-  <h1>Hello Fabiano</h1>
+  <>
+    <h1>Hello Fabiano</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
