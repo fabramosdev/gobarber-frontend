8b5ba2d51e2d6616ceb9eab64a3cfc7c0224f405
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-08-21 14:26:47 -0300

    Input & Button components isolated

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -25,57 +26,23 @@ export const Content = styled.div`
   }
 
   h1 {
-    margin-bottom: 24px
-  }
-
-  input {
-    background: #232129;
-    border-radius: 10px;
-    border: 2px solid #232129;
-    padding: 16px;
-    width: 100%;
-    color: #F4EDE8;
-
-    &::placeholder {
-      color: #666360;
-    }
-
-    & + input {
-      margin-top: 8px;
-    }
-  }
-
-  button {
-    background: #FF9000;
-    height: 56px;
-    border-radius: 10px;
-    border: 0;
-    padding: 0 16px;
-    color: #312e38;
-    width: 100%;
-    font-weight: 500;
-    margin-top: 16px;
-    transition: background-color 0.2s;
-
-    &:hover {
-      background: ${shade(0.2, '#FF9000')}
-    }
+    margin-bottom: 24px;
   }
 
   a {
-    color: #F4EDE8;
+    color: #f4ede8;
     display: block;
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
 
     &:hover {
-      color: ${shade(0.2, '#F3EDE8')}
+      color: ${shade(0.2, '#F3EDE8')};
     }
   }
 
   > a {
-    color: #FF9000;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -89,7 +56,7 @@ export const Content = styled.div`
     }
 
     &:hover {
-      color: ${shade(0.2, '#FF9000')}
+      color: ${shade(0.2, '#FF9000')};
     }
   }
 `;
