7f775a8af696287ef53fb2e550a5bf94e26d0145
Fabiano Ramos <fabiano.suporteinfo@gmail.com>
2020-09-02 18:11:51 -0300

    Login Screen and Auth - Mission accomplished

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,7 +6,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -30,6 +30,7 @@ const SignIn: React.FC = () => {
 
   const { signIn } = useAuth();
   const { addToast } = useToast();
+  const history = useHistory();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -51,23 +52,26 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         });
+
+        history.push('/dashboard');
       } catch (err) {
-        const errors = getValidationErrors(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
 
-        // console.log(errors);
-        return;
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description:
+            'Ocorreu um erro ao fazer o login. Cheque suas credenciais',
+        });
       }
-      addToast({
-        type: 'error',
-        title: 'Erro na autenticação',
-        description:
-          'Ocorreu um erro ao fazer o login. Cheque suas credenciais',
-      });
-      // console.log(data);
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
 
   return (
